039c2bd363e6a28710d4b6524e070bd73a5f26b5
dilane3 <komboudilane125@gmail.com>
2021-10-10 20:50:14 +0100

    adding a new section for uploading music

--- src/components/tools/MusikList.jsx
+++ src/components/tools/MusikList.jsx
@@ -1,30 +1,74 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import styles from '../app.module.css'
 import Musik from './Musik'
 import MusikContext from '../contexts/musikContext'
+import Comparator from 'easy-comparator'
+
+const compare = new Comparator()
 
 const MusikList = () => {
   const {musiks} = useContext(MusikContext)
+  const [navigation, setNavigation] = useState("musik-list")
+
+  const navigate = () => {
+    if (compare.equal(navigation, "musik-list"))
+      setNavigation("upload-musik")
+    else
+      setNavigation("musik-list")
+  }
 
   return (
     <aside className={styles.aside}>
       <div className={styles.musikHeader}>
-        <span className="badge badge-success">6</span>
-        <span>Musiques</span>
+        <div>
+          <span className="badge badge-success">{musiks.length}</span>
+          <span>Musiques</span>
+        </div>
+        
+        <div>
+          <i 
+            className={`bi bi-${compare.equal(navigation, "musik-list") ? "upload":"justify"}`}
+            onClick={() => navigate()}
+          >
+          </i>
+        </div>
       </div>
+      
       <div className={styles.musikList}>
         {
-          musiks.map(musik => (
-            <React.Fragment key={musik.id}>
-              <Musik
-                musik={musik}
-              />
-            </React.Fragment>
-          ))
+          compare.equal(navigation, "musik-list") ? (
+            <List musiks={musiks} />
+          ):(
+            <UploadMusik />
+          )
         }
-
       </div>
     </aside>
+  )
+}
+
+const List = ({musiks}) => {
+  return (
+    <>
+      {
+        musiks.map(musik => (
+          <React.Fragment key={musik.id}>
+            <Musik
+              musik={musik}
+            />
+          </React.Fragment>
+        ))
+      }
+
+    </>
+  )
+}
+
+const UploadMusik = () => {
+  return (
+    <section className={styles.uploadMusik}>
+      {"hello"}
+    </section>
   )
 }
 
