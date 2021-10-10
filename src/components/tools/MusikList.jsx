76dab20aed3f3c403416ff4f36036c2764e01b95
dilane3 <komboudilane125@gmail.com>
2021-10-10 22:19:32 +0100

    adding state to the UploadMusik component

--- src/components/tools/MusikList.jsx
+++ src/components/tools/MusikList.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react'
+import React, {useContext, useState, useRef, useEffect} from 'react'
 import styles from '../app.module.css'
 import styles2 from '../upload.module.css'
 import Musik from './Musik'
@@ -66,17 +66,39 @@ const List = ({musiks}) => {
 }
 
 const UploadMusik = () => {
+  const [file, setFile] = useState(null)
+  const [title, setTitle] = useState("")
+  const [author, setAuthor] = useState("")
+
+  const inputFileRef = useRef()
+
+  const handleChangeFile = (value) => {
+    setFile(value)
+    setTitle(value.name.split(".")[0])
+  }
+
   return (
     <section className={styles.uploadMusik}>
       <span className={styles2.uploadeMusikTitle}>UPLOAD MUSIC</span>
 
-      <div className={styles2.uploadMusikTrigger}>Select a music</div>
+      <div className={styles2.uploadMusikTrigger} onClick={() => inputFileRef.current.click()}>Select a music</div>
+      <span className={styles2.indicator}>
+        {
+          !compare.equal(file, null) && "music selected"
+        }
+      </span>
+      <input 
+        ref={inputFileRef} 
+        type="file" 
+        className="d-none"
+        onChange={(e) => handleChangeFile(e.target.files[0])}
+      />
 
       <form className={styles2.uploadMusikForm}>
         <div className={styles2.uploadMusikFormField}>
-          <input type="text" placeholder="Title" />
+          <input value={title} type="text" placeholder="Title" onChange={(e) => setTitle(e.target.value)} />
 
-          <input type="text" placeholder="Author" />
+          <input value={author} type="text" placeholder="Author" onChange={(e) => setAuthor(e.target.value)} />
         </div>
 
         <button className={styles2.uploadMusikButton}>Save</button>
